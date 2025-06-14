67f5f536b6a7339fbd160ea398f141ac9c97695c
rxmann <k.romann413@gmail.com>
2025-06-14 22:43:25 +0545

    fix(): structure x3

--- nvim/lua/core/vim-keymaps.lua
+++ nvim/lua/core/vim-keymaps.lua
@@ -5,7 +5,3 @@ vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
-
-vim.keymap.set('n', '<Space>ff', ':Telescope find_files<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<Space>fg', ':Telescope live_grep<CR>', { noremap = true, silent = true })
-
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,29 +1,33 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope-ui-select.nvim",
-    },
-    config = function()
-      local telescope = require("telescope")
-      local builtin = require("telescope.builtin")
-
-      telescope.setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({})
-          },
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.5",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope-ui-select.nvim",
         },
-      })
+        config = function()
+            local telescope = require("telescope")
+            local builtin = require("telescope.builtin")
 
-      telescope.load_extension("ui-select")
+            telescope.setup({
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown({})
+                    },
+                },
+            })
 
-      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-      vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
-    end,
-  },
+            telescope.load_extension("ui-select")
+
+            -- Ctrl+P to find files
+            vim.keymap.set("n", "<leader>ff", builtin.find_files, { noremap = true, silent = true })
+
+            -- Ctrl+Shift+P to live grep
+            vim.keymap.set("n", "<leader>lg", builtin.live_grep, { noremap = true, silent = true })
+
+            -- <leader>ff to show old files
+            vim.keymap.set("n", "<leader>fo", builtin.oldfiles, { noremap = true, silent = true })
+        end,
+    },
 }
-
