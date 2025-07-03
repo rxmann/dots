a4bda48273591481581ef13568814c4888d9fcc5
rxmann <k.romann413@gmail.com>
2025-07-03 19:12:47 +0545

    nvim setup

--- nvim/lua/plugins/poimandres.lua
+++ nvim/lua/plugins/poimandres.lua
@@ -1,17 +1,38 @@
 return {
-    'olivercederborg/poimandres.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-        require('poimandres').setup {
-            -- leave this setup function empty for default config
-            -- or refer to the configuration section
-            -- for configuration options
-        }
-    end,
+	"olivercederborg/poimandres.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		-- Local state
+		local bg_transparent = false
 
-    -- optionally set the colorscheme within lazy config
-    init = function()
-        vim.cmd("colorscheme poimandres")
-    end
+		-- Setup function to apply the theme with current transparency setting
+		local function apply_poimandres()
+			require("poimandres").setup({
+				bold_vert_split = true,
+				dim_nc_background = false,
+				disable_background = bg_transparent,
+				disable_float_background = false,
+				disable_italics = false,
+			})
+			vim.cmd("colorscheme poimandres")
+		end
+
+		-- Initial apply
+		apply_poimandres()
+
+		-- Toggle function for background transparency
+		local function toggle_transparency()
+			bg_transparent = not bg_transparent
+			apply_poimandres()
+		end
+
+		-- Keybinding to toggle background
+		vim.keymap.set("n", "<leader>bg", toggle_transparency, { noremap = true, silent = true })
+	end,
+
+	-- Optional init function (can be removed since it's handled in config now)
+	init = function()
+		-- Do nothing here; moved to config
+	end,
 }
