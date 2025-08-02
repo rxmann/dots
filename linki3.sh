3e01dff366eede3c941e7d14188d2880d16c6133
rxmann <k.romann413@gmail.com>
2025-08-02 23:14:45 +0545

    fix(): i3 link script

--- linki3.sh
+++ linki3.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
+set -e
 
 DOTS=~/projects/dots
 CONFIG=~/.config
 
-echo "Linking i3..."
-ln -sf "$DOTS/i3" "$CONFIG/i3"
+link() {
+    local src=$1
+    local dst=$2
 
-echo "Linking polybar..."
-ln -sf "$DOTS/polybar" "$CONFIG/polybar"
+    echo "Linking $dst → $src"
+    if [ -e "$dst" ] || [ -L "$dst" ]; then
+        rm -rf "$dst"
+    fi
 
-echo "Linking rofi..."
-ln -sf "$DOTS/rofi" "$CONFIG/rofi"
+    ln -s "$src" "$dst"
+}
+
+link "$DOTS/i3" "$CONFIG/i3"
+link "$DOTS/polybar" "$CONFIG/polybar"
+link "$DOTS/rofi" "$CONFIG/rofi"
+link "$DOTS/tmux/.tmux.conf" ~/.tmux.conf
+link "$DOTS/nvim" "$CONFIG/nvim"
 
 
 echo "Linking picom..."
